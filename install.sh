10bff2e3efcbbf49dc33e6aa4392cdc1b906d8a3
Still <likiblack@gmail.com>
2024-10-04 10:50:53 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ interval = $3 * 60
 
 while True:
     # Compose the message with the server hostname, IP address, and database file
-    message = f\"Server: {hostname} ({ip_address})\nDatabase file:\"
+    message = f\"Server: {hostname} ({ip_address})\n\"
     with open(db_path, \"rb\") as file:
         bot.send_document(chat_id, file, caption=message)
     # Sleep for the specified interval
